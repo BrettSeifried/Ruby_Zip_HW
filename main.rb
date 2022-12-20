ad0dd3a012c4c64bf4d3383b3bf95a62d2e3781e
Brett Seifried <seifriedbrett@gmail.com>
2022-12-20 10:34:03 -0800

    working area code look up

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
+# Yes or no
+# if Y which city?
+# if match city, give area code
+# loops back asking for city
+
 dial_book = {
   "newyork" => "212",
   "newbrunswick" => "732",
@@ -10,15 +15,17 @@ dial_book = {
   "orlando" => "407",
   "lancaster" => "717"
 }
- 
+
 # Get city names from the hash
 def get_city_names(somehash)
 # Write code here
+  somehash.keys
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
 # Write code here
+  somehash[key]
 end
  
 # Execution flow
@@ -29,5 +36,14 @@ loop do
   if answer != "y"
     puts "If you got this far! send me an E-mail at seifriedbrett@gmail.com!"
     break
+  else
+    puts get_city_names(dial_book)
+    puts "Which area code would you like to see?"
+    city_input = gets.chomp.downcase
+    if dial_book.include?(city_input)
+      puts "The area code for #{city_input} is #{get_area_code(dial_book, city_input)}"
+    else
+      puts "That city is not in our dictionary"
+    end
   end
 end
